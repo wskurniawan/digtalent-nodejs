c52bc6dc0a463b2c55d7ff89c7228d057da7d0d9
Wisnu Kurniawan <kurniawan.wisnu23@gmail.com>
2020-10-11 21:19:58 +0700

    init product insert and select

--- database.js
+++ database.js
@@ -7,3 +7,34 @@ export function connectDB() {
     database: 'product_catalog'
   })
 }
+
+/**
+ * 
+ * @param {mysql.Connection} dbConnection 
+ * @param {string} name 
+ * @param {number} price 
+ * @param {string} photo 
+ */
+export function insertProduct(dbConnection, name, price, photo) {
+  dbConnection.query('INSERT INTO product SET ?', { name, price, photo }, (err) => {
+    if(err) {
+      console.log(err)
+      throw err
+    }
+  })
+}
+
+/**
+ * 
+ * @param {mysql.Connection} dbConnection 
+ */
+export function getProduct(dbConnection) {
+  dbConnection.query('SELECT * FROM product', (err, result) => {
+    if(err) {
+      console.log(err)
+      throw err
+    }
+    console.log(result)
+    return result
+  })
+}
