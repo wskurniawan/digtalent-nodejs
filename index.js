fa1d1a7ac8aa7009b0365574304220813882dc4b
Wisnu Kurniawan <kurniawan.wisnu23@gmail.com>
2020-10-11 19:55:20 +0700

    init login, search-product, default-error page

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
 
+const __dirname = path.resolve()
+
 const app = express()
 
 app.set('views', path.join(__dirname, '/layout'))
@@ -9,7 +11,22 @@ app.set('view engine', 'html')
 app.engine('html', hbs.__express)
 
 // app route
+app.get('/login', (req, res, next) => {
+  res.render('login')
+})
 
+app.get('/search-product', (req, res, next) => {
+  res.render('search-product')
+})
+
+app.use((req, res, next) => {
+  return next(new Error('404: Halaman tidak ditemukan'))
+})
+
+app.use((err, req, res, next) => {
+  console.log(err.message)
+  res.render('default-error')
+})
 
 app.listen(8000, () => {
   console.log('app listen on port 8000')
